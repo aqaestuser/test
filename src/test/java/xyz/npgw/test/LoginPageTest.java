310043c50ec1db8e4ac9a67a5519c8ab9d6c3495
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-03-24 13:30:31 +0200

    [!] reorder imports & checkstyle (#89)

--- src/test/java/xyz/npgw/test/LoginPageTest.java
+++ src/test/java/xyz/npgw/test/LoginPageTest.java
@@ -5,10 +5,10 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import org.testng.annotations.Test;
-import xyz.npgw.test.page.DashboardPage;
 import xyz.npgw.test.common.Constants;
-import xyz.npgw.test.page.LoginPage;
 import xyz.npgw.test.common.base.BaseTest;
+import xyz.npgw.test.page.DashboardPage;
+import xyz.npgw.test.page.LoginPage;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
