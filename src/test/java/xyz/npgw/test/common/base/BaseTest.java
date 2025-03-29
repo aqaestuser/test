4c4003a2e60ce7fb854e638bbfbba4bbebb6a76d
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-03-29 09:57:37 +0200

    Fix: gracefully close playwright on unsupported browser (#142)

--- src/test/java/xyz/npgw/test/common/base/BaseTest.java
+++ src/test/java/xyz/npgw/test/common/base/BaseTest.java
@@ -44,6 +44,9 @@ public abstract class BaseTest {
             playwright = Playwright.create();
             browser = BrowserFactory.getBrowser(playwright, this.browserType);
         } catch (IllegalArgumentException e) {
+            if (playwright != null) {
+                playwright.close();
+            }
             LOGGER.error("Unsupported browser: {}", browserType);
             System.exit(1);
         } catch (RuntimeException e) {
