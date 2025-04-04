0e0517dc8543f3c7773fd10a408328d7b434fbc3
IriSamo <i.samosuk@gmail.com>
2025-04-04 11:27:23 -0400

    FR

--- src/test/java/xyz/npgw/test/page/base/SystemAdministrationWithTableBasePage.java
+++ src/test/java/xyz/npgw/test/page/base/SystemAdministrationWithTableBasePage.java
@@ -1,12 +1,22 @@
 package xyz.npgw.test.page.base;
 
 import com.microsoft.playwright.Page;
+import xyz.npgw.test.page.component.HeaderComponent;
+import xyz.npgw.test.page.component.SystemAdministrationMenuComponent;
 import xyz.npgw.test.page.component.TableComponent;
 
-public abstract class SystemAdministrationWithTableBasePage extends SystemAdministrationBasePage{
+public abstract class SystemAdministrationWithTableBasePage extends BasePage{
 
     public SystemAdministrationWithTableBasePage(Page page) {
         super(page);
+    }
+
+    public HeaderComponent getHeader() {
+        return new HeaderComponent(getPage());
+    }
+
+    public SystemAdministrationMenuComponent getSystemAdministrationMenuComponent() {
+        return new SystemAdministrationMenuComponent(getPage());
     }
 
     public TableComponent getTable() {
