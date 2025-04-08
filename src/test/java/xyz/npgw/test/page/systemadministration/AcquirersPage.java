3b5ed9537d548943e4d3af95a6e481ac6b449f4c
LaysanNova <114321861+LaysanNova@users.noreply.github.com>
2025-04-07 21:00:54 -0600

    Add ATC_11.003.00 При нажатии на поле "Status" выпадает список c опциями "ALL/Active/Inactive" (#234)
    Add ATC_11.003.00 При нажатии на поле "Status" выпадает список c опциями "ALL/Active/Inactive"

--- src/test/java/xyz/npgw/test/page/systemadministration/AcquirersPage.java
+++ src/test/java/xyz/npgw/test/page/systemadministration/AcquirersPage.java
@@ -14,15 +14,25 @@ public class AcquirersPage extends SystemAdministrationBasePage {
     private final Locator resetFilterButton = locator("svg[data-icon='xmark']");
     @Getter
     private final Locator applyFilterButton = locator("svg[data-icon='filter']");
-    @Getter
-    private final Locator selectAcquirerLabel = labelExact("Select acquirer");
-    @Getter
-    private final Locator statusLabel = labelExact("Status");
+
+
     @Getter
     private final Locator acquirersListHeader = textExact("Acquirers list");
     private final Locator acquirersList = locator("div[data-slot='base'] li");
+
+
+    @Getter
+    private final Locator selectAcquirerLabel = labelExact("Select acquirer");
     private final Locator selectAcquirerPlaceholder = placeholder("Search");
     private final Locator dropdownAcquirerList = locator("div[data-slot='content'] li");
+
+    @Getter
+    private final Locator statusLabel = labelExact("Status");
+    private final Locator acquirerStatusPlaceholder = locator("div[data-slot='innerWrapper'] span");
+    @Getter
+    private final Locator dropdownAcquirerStatusList = locator("div[data-slot='listbox']");
+    @Getter
+    private final Locator acquirerStatusOptions = option(dropdownAcquirerStatusList);
 
     public AcquirersPage(Page page) {
         super(page);
@@ -47,4 +57,13 @@ public class AcquirersPage extends SystemAdministrationBasePage {
 
         return dropdownAcquirerList;
     }
+
+    @Step("Click Status placeholder")
+    public AcquirersPage clickAcquirerStatusPlaceholder() {
+        selectAcquirerLabel.waitFor();
+        acquirerStatusPlaceholder.click();
+
+        return this;
+    }
+
 }
