925c5e77a87626297c5d015cf1885a32354030d0
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-04-15 16:06:21 +0300

    Fix: ignore AddAcquirerDialogTest class (#296)

--- src/test/java/xyz/npgw/test/run/AddAcquirerDialogTest.java
+++ src/test/java/xyz/npgw/test/run/AddAcquirerDialogTest.java
@@ -6,6 +6,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.common.provider.TestDataProvider;
@@ -18,6 +19,7 @@ import java.util.List;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.testng.Assert.assertEquals;
 
+@Ignore
 public class AddAcquirerDialogTest extends BaseTest {
 
     @Test()
