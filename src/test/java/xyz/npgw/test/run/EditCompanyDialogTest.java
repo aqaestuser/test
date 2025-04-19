87c9e7321ea6239d22b50aff3973e7d0e56c6dd9
aqaestuser <144237758+aqaestuser@users.noreply.github.com>
2025-04-19 15:33:24 +0300

    Fix: testEditCompanyInfoAndSave - bug gone (#316)

--- src/test/java/xyz/npgw/test/run/EditCompanyDialogTest.java
+++ src/test/java/xyz/npgw/test/run/EditCompanyDialogTest.java
@@ -5,8 +5,6 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
-import org.opentest4j.AssertionFailedError;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import xyz.npgw.test.common.base.BaseTest;
 import xyz.npgw.test.page.DashboardPage;
@@ -16,7 +14,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class EditCompanyDialogTest extends BaseTest {
 
-    @Test()
+    @Test
     @TmsLink("266")
     @Epic("System/Companies and business units")
     @Feature("Edit company")
