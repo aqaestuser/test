5d0b73a220d8a5c81caedeca747f45653fe85af9
LaysanNova <114321861+LaysanNova@users.noreply.github.com>
2025-05-15 08:52:34 -0600

    ATC_11.000.10	Verify that Acquirer with status 'Active/Inactive' is displayed correctly in the list (#558)
    * add 11.000.10

--- src/test/java/xyz/npgw/test/page/system/UserTableComponent.java
+++ src/test/java/xyz/npgw/test/page/system/UserTableComponent.java
@@ -17,48 +17,48 @@ public class UserTableComponent extends TableComponent {
 
     @Step("Click 'Edit user'")
     public EditUserDialog clickEditUserButton(String email) {
-        getTableRow(email).getByTestId("EditUserButton").click();
+        getRowByPrimaryColumn(email).getByTestId("EditUserButton").click();
 
         return new EditUserDialog(getPage());
     }
 
     @Step("Click 'Deactivate user' button")
     public ChangeUserActivityDialog clickDeactivateUserButton(String email) {
-        getTableRow(email).locator("//*[@data-icon='ban']/..").click();
+        getRowByPrimaryColumn(email).locator("//*[@data-icon='ban']/..").click();
 
         return new ChangeUserActivityDialog(getPage());
     }
 
     @Step("Click 'Activate user' button")
     public ChangeUserActivityDialog clickActivateUserButton(String email) {
-        getTableRow(email).locator("//*[@data-icon='check']/..").click();
+        getRowByPrimaryColumn(email).locator("//*[@data-icon='check']/..").click();
 
         return new ChangeUserActivityDialog(getPage());
     }
 
     @Step("Click 'Reset user password' button")
     public ResetUserPasswordDialog clickResetUserPasswordButton(String email) {
-        getTableRow(email).getByTestId("ResetUserPasswordButton").click();
+        getRowByPrimaryColumn(email).getByTestId("ResetUserPasswordButton").click();
 
         return new ResetUserPasswordDialog(getPage());
     }
 
     public Locator getUserRole(String email) {
         int columnIndex = getColumnHeaderIndexByName("User role");
-        Locator row = getTableRow(email);
+        Locator row = getRowByPrimaryColumn(email);
 
         return row.getByRole(AriaRole.GRIDCELL).or(row.getByRole(AriaRole.ROWHEADER)).nth(columnIndex);
     }
 
     public Locator getUserStatus(String email) {
         int columnIndex = getColumnHeaderIndexByName("Status");
-        Locator row = getTableRow(email);
+        Locator row = getRowByPrimaryColumn(email);
 
         return row.getByRole(AriaRole.GRIDCELL).or(row.getByRole(AriaRole.ROWHEADER)).nth(columnIndex);
     }
 
     public Locator getUserActivityIcon(String email) {
-        return getTableRow(email).getByTestId("ChangeUserActivityButton").locator("svg");
+        return getRowByPrimaryColumn(email).getByTestId("ChangeUserActivityButton").locator("svg");
     }
 
     @Step("Deactivate user")
